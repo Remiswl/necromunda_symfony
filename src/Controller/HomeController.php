4efb7c724b6772b3cb1b6cf963f971d70fa261d6
Remiswl <r.schwall@hotmail.fr>
2021-03-23 14:28:07 +0100

    :art: add CSS, resources, server configuration, Home page, Resources page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+	/**
+	* @Route("/resources", name="resources")
+	*/
+    public function resources(): Response
+    {
+        return $this->render('home/resources.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+}

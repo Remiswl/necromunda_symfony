055ca5e9f51f2859c158f917f5fb0bce6f182316
Remiswl <r.schwall@hotmail.fr>
2021-04-10 18:45:24 +0200

    :construction: admin - add a button to scroll to the top

--- migrations/Version20210410145238.php
+++ migrations/Version20210410145238.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210410104003 extends AbstractMigration
+final class Version20210410145238 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,14 +20,12 @@ final class Version20210410104003 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        //$this->addSql('CREATE TABLE gangers_img (id INT AUTO_INCREMENT NOT NULL, house_id INT NOT NULL, path VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        //$this->addSql('ALTER TABLE territories DROP d66roll, CHANGE description description TEXT NOT NULL');
+        $this->addSql('ALTER TABLE territories CHANGE description description TEXT NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE gangers_img');
         $this->addSql('ALTER TABLE territories ADD d66roll INT NOT NULL, CHANGE description description TEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }

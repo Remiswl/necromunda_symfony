8968a223e995c90f335c8fe50a5b8ba48ba66beb
Remiswl <r.schwall@hotmail.fr>
2021-07-11 23:34:58 +0200

    :art: start working on battles

---
+++ src/Controller/BattleController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\GangsRepository;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class BattleController extends AbstractController
+{
+    private $gangsRepository;
+
+    public function __construct(
+        GangsRepository $gangsRepository
+    ) {
+        $this->gangsRepository = $gangsRepository;
+    }
+
+    /**
+     * @Route("/battle1", name="battle1")
+     */
+    public function battle1(): Response
+    {
+        $gangsNames = $this->gangsRepository->findAll();
+
+        return $this->render('battle/battle1.html.twig', [
+            'gangs' => $gangsNames,
+        ]);
+    }
+
+    /**
+     * @Route("/battle2", name="battle2")
+     */
+    public function battle2(Request $request): Response
+    {
+        dd($request->request->get('gangs'));
+
+        $gangsNames = $this->gangsRepository->findAll();
+
+        return $this->render('battle/battle2.html.twig', [
+            'gangs' => $gangsNames,
+        ]);
+    }
+
+    /**
+     * @Route("/result", name="result")
+     */
+    public function result(): Response
+    {
+        $gangsNames = $this->gangsRepository->findAll();
+
+        return $this->render('battle/result.html.twig', [
+            'gangs' => $gangsNames,
+        ]);
+    }
+}

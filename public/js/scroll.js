434641c71465f8158cbd2207ab835b10e42176b9
Remiswl <r.schwall@hotmail.fr>
2021-07-14 17:08:31 +0200

    :art: remove useless comments

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -12,16 +12,14 @@ function scrollFunction() {
 }
 
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
-// When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener('click', () => {
     topFunction();
 });
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
     scrollFunction();
 };
